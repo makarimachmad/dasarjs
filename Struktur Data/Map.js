8486fd6a85f13d945d68c7241bb1054af8cbd8a0
achmad makarim widyanto <makarimachmad@gmail.com>
2021-05-10 13:16:03 +0700

    merapikan kodingan:

--- Struktur Data/Map.js
+++ Struktur Data/Map.js
@@ -1,3 +1,4 @@
+//map
 let visitsCountMap = new Map(); // Menyimpan daftar user
  
 function countUser(user) {
@@ -11,3 +12,22 @@ countUser(jonas);                // Menambahkan user "Jonas"
 jonas = null;                    // Data object "Jonas" dihapus
  
 console.log(visitsCountMap);
+
+//weakmap
+let visitsCountMap = new WeakMap(); // Menyimpan daftar user
+
+function countUser(user) {
+    let count = visitsCountMap.get(user) || 0;
+    visitsCountMap.set(user, count + 1);
+}
+
+let jonas = { name: "Jonas" };
+countUser(jonas);                // Menambahkan user "Jonas"
+
+jonas = null;                    // Data object "Jonas" dihapus
+
+console.log(visitsCountMap);
+
+/* output
+WeakMap { <items unknown> }
+*/
